399bf51f0979ba3218c3c19c6f200fe33f65a43a
Rymdenfototo <123206994+Rymdenfototo@users.noreply.github.com>
2023-10-28 20:36:00 +0200

    Add files via upload

---
+++ verification.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verifikation</title>
+</head>
+
+<body>
+    <h1>Ange din verifikationskod</h1>
+    <form action="verify.php" method="post">
+        Verifikationskod: <input type="text" name="verificationCode" required><br>
+        <input type="submit" value="Verifiera">
+    </form>
+</body>
+
+</html>
